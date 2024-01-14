55fa9b27ad04bf50af7d59f8ad8ac486896b6df7
ibiscum <ulf.luecking@gmail.com>
2024-01-14 08:33:58 +0100

    ch07.

--- ch07/go.mod
+++
@@ -1,5 +0,0 @@
-module example.com/ch07
-
-go 1.21.5
-
-require golang.org/x/sync v0.6.0
--- ch07/goClosureCorrect/goClosureCorrect.go
+++ ch07/goClosureCorrect/goClosureCorrect.go
@@ -1,5 +1,3 @@
-//go:build ignore
-
 package main
 
 import (
@@ -9,9 +7,18 @@ import (
 
 func main() {
 	for i := 0; i <= 20; i++ {
+		i := i
 		go func() {
 			fmt.Print(i, " ")
 		}()
+	}
+	time.Sleep(time.Second)
+	fmt.Println()
+
+	for i := 0; i <= 20; i++ {
+		go func(x int) {
+			fmt.Print(x, " ")
+		}(i)
 	}
 	time.Sleep(time.Second)
 	fmt.Println()
